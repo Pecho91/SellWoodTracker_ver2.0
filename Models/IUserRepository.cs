fe8de3046f88b8a2144c1ebd6fa013ed50611dae
andrija.pecaric@gmail.com <andrija.pecaric@gmail.com>
2024-04-11 23:27:33 +0200

    update

---
+++ Models/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellWoodTracker_ver2_0.Models
+{
+    public interface IUserRepository
+    {
+        bool AuthenticateUser(NetworkCredential credential);
+        void Add(UserModel userModel);
+        void Edit(UserModel userModel);
+        void Remove(int id);
+        UserModel GetById(int id);
+        UserModel GetByUsername(string username);
+        IEnumerable<UserModel> GetByAll();
+        
+        //TODO , 22min. Add new project repository (data layer)
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SellWoodTracker_ver2_0.Models
+{
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name {  get; set; }
+        public string LastName {  get; set; }
+        public string Email { get; set; }
+        
+
+        
+    }
+}
