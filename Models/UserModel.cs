9433bed3ee7d08e481eba031d828dd35ce3927e8
andrija.pecaric@gmail.com <andrija.pecaric@gmail.com>
2024-04-12 14:51:57 +0200

    update

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -15,8 +15,6 @@ namespace SellWoodTracker_ver2_0.Models
         public string Name {  get; set; }
         public string LastName {  get; set; }
         public string Email { get; set; }
-        
-
-        
+              
     }
 }
